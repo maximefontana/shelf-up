f181aa15b64d208533e6468733629f248bee50d2
maxime fontana <maximefontana@hotmail.com>
2018-11-12 18:21:43 +0100

    added pundit gem and necessary files in app policy and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
+# frozen_string_literal: true
+
+# rubocop:disable all
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
+  include Pundit
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
 end
