bae039503d8a65c10fcc4c47432c63aca8c293ab
Bella Cha <cha.maria.belen@gmail.com>
2018-11-13 12:12:00 +0100

    generated bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'bookings/show'
+  get 'bookings/new'
+  get 'bookings/create'
   devise_for :users
 
   root to: 'pages#home'
 
   resources :users, only: [:show]
   resources :stores,  only: [:index, :show]
-
+  resources :bookings, only: [:show, :new, :create]
 end
