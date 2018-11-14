359cef7a627dff797422f7fe4f9fdc4ccddc4674
Bella Cha <cha.maria.belen@gmail.com>
2018-11-14 14:43:17 +0100

    added search bar and category to stores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_153537) do
+ActiveRecord::Schema.define(version: 2018_11_14_132520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_11_13_153537) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "photo"
+    t.string "category"
     t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
