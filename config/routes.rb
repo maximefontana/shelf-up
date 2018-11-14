8f75674ee9b3cc2771ba16d14971367609d705a7
Bella Cha <cha.maria.belen@gmail.com>
2018-11-14 13:54:08 +0100

    added stores to user page

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
     root 'users#show', as: :authenticated_root
   end
 
-  root to: 'pages#home'
+  get 'home', to: 'pages#home', as: :home
 
   resources :users, only: [:show, :update]
 
-  resources :stores,  only: [:index, :show, :new, :create, :edit, :update] do
+  resources :stores do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:show]
