9348288a8ff40f444fb2ad43804334561cc327ef
michael mudge <michaeltmudge@gmail.com>
2018-11-16 10:42:46 +0100

    nav bar stuff not fully working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home, raise: false
 
-  layout 'homepage'
-
   def home
     @user = current_user
   end
