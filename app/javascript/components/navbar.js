59c5a35641b0b8fe98b5a2287856373a3ec18020
maxime fontana <maximefontana@hotmail.com>
2018-11-21 10:53:28 +0100

    navbar homepage links change color

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,6 +1,7 @@
 function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-homepage');
-  const links = document.querySelectorAll('.navbar-homepage-wagon-link')
+  const links = document.querySelectorAll('.navbar-homepage-wagon-link');
+  const logo = document.querySelector('.nav-homepage-logo');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
@@ -9,12 +10,16 @@ function initUpdateNavbarOnScroll() {
         links[0].classList.add('navbar-homepage-black-link');
         links[1].classList.remove('navbar-wagon-link');
         links[1].classList.add('navbar-homepage-black-link');
+        logo.classList.remove('nav-homepage-logo');
+        logo.classList.add('navbar-homepage-logo-black');
       } else {
         navbar.classList.remove('navbar-homepage-white');
         links[0].classList.add('navbar-wagon-link');
         links[0].classList.remove('navbar-homepage-black-link');
         links[1].classList.add('navbar-wagon-link');
         links[1].classList.remove('navbar-homepage-black-link');
+        logo.classList.add('nav-homepage-logo');
+        logo.classList.remove('navbar-homepage-logo-black');
       }
     });
   }
